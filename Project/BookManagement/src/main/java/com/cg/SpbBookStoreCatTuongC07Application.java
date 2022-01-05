39c233fb5cfc862e5e69f1502e38d9874816d316
THAO_VT\THAO_VT <vothao.tin@gmail.com>
2022-01-05 10:46:31 +0700

    up

--- Project/BookManagement/src/main/java/com/cg/SpbBookStoreCatTuongC07Application.java
+++ Project/BookManagement/src/main/java/com/cg/SpbBookStoreCatTuongC07Application.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpbBookStoreCatTuongC07Application {
+public class  SpbBookStoreCatTuongC07Application {
 
     public static void main(String[] args) {
         SpringApplication.run(SpbBookStoreCatTuongC07Application.class, args);
